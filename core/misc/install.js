73e13353dfbc76ea2d223df79306463a27704e8b
e2tha-e <ee@electric-eloquence.io>
2016-12-30 18:52:25 -0500

    install file updates

--- core/misc/install.js
+++ core/misc/install.js
@@ -1,46 +1,60 @@
 'use strict';
 
 const exec = require('child_process').exec;
-const fs = require('fs');
+const fs = require('fs-extra');
 const path = require('path');
-
-const copy = require('./copy');
 
 const excludesDir = 'node_modules/fepper/excludes';
 
-new Promise(function (resolve) {
-  copy.file('conf.yml', excludesDir, resolve);
-})
-.then(function () {
-  return new Promise(function (resolve) {
-    copy.dir('extend', excludesDir, resolve);
-  });
-})
-.then(function () {
-  return new Promise(function (resolve) {
-    copy.file('patternlab-config.json', excludesDir, resolve);
-  });
-})
-.then(function () {
-  return new Promise(function (resolve) {
-    copy.file('pref.yml', excludesDir, resolve);
-  });
-})
-.then(function () {
-  var binGulp = path.resolve('node_modules', '.bin', 'gulp');
-  exec(`${binGulp} --gulpfile node_modules/fepper/tasker.js install`, (err, stdout, stderr) => {
-    if (err) {
-      throw err;
-    }
+var confFile = 'conf.yml';
+var confFileSrc = path.resolve(excludesDir, confFile);
+if (fs.existsSync(confFileSrc)) {
+  fs.copySync(confFileSrc, confFile);
+}
 
-    fs.writeFileSync('install.log', stdout);
-    if (stderr) {
+var extendDir = 'extend';
+var extendDirSrc = path.resolve(excludesDir, extendDir);
+if (fs.existsSync(extendDirSrc)) {
+  fs.copySync(extendDirSrc, extendDir);
+}
 
-      /* eslint-disable no-console */
-      console.log(stderr);
+var fepperFile = 'fepper.command';
+var fepperFileSrc = path.resolve(excludesDir, fepperFile);
+if (fs.existsSync(fepperFileSrc)) {
+  fs.copySync(fepperFileSrc, fepperFile);
+}
 
-      /* eslint-enable no-console */
-      fs.appendFileSync('install.log', stderr);
-    }
-  });
+var plConfFile = 'patternlab-config.json';
+var plConfFileSrc = path.resolve(excludesDir, plConfFile);
+if (fs.existsSync(plConfFileSrc)) {
+  fs.copySync(plConfFileSrc, plConfFile);
+}
+
+var prefFile = 'pref.yml';
+var prefFileSrc = path.resolve(excludesDir, prefFile);
+if (fs.existsSync(prefFileSrc)) {
+  fs.copySync(prefFileSrc, prefFile);
+}
+
+var publicDir = 'public';
+var publicDirSrc = path.resolve(excludesDir, publicDir);
+if (fs.existsSync(publicDirSrc)) {
+  fs.copySync(publicDirSrc, publicDir);
+}
+
+var binGulp = path.resolve('node_modules', '.bin', 'gulp');
+exec(`${binGulp} --gulpfile node_modules/fepper/tasker.js install`, (err, stdout, stderr) => {
+  if (err) {
+    throw err;
+  }
+
+  fs.writeFileSync('install.log', stdout);
+  if (stderr) {
+
+    /* eslint-disable no-console */
+    console.log(stderr);
+
+    /* eslint-enable no-console */
+    fs.appendFileSync('install.log', stderr);
+  }
 });
