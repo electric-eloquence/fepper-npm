3ce7847f8cff1c7affbb63fa8abd7d4c51c93d6d
e2tha-e <ee@electric-eloquence.io>
2016-12-31 06:12:30 -0500

    consulting conf to install extend, public and source dirs

--- tasker/install.js
+++ tasker/install.js
@@ -1,27 +1,37 @@
 /**
- * Using gulp to copy the source dir because we need to parse patternlab-config.json to know the destination.
+ * Using gulp to copy these dirs because we need to parse patternlab-config.json to know their destinations.
  */
 'use strict';
-
-const srcDir = global.conf.ui.paths.source;
 
 const fs = require('fs-extra');
 const gulp = require('gulp');
 
 const utils = require('../core/lib/utils');
 
+const extendDir = global.conf.extend_dir;
+const publicDir = global.conf.ui.paths.public.root;
+const sourceDir = global.conf.ui.paths.source.root;
+
 gulp.task('install:copy', function (cb) {
-  if (!fs.existsSync(utils.pathResolve(srcDir.root))) {
+  if (!fs.existsSync(utils.pathResolve(extendDir))) {
+    return gulp.src('./excludes/extend/**')
+      .pipe(gulp.dest(utils.pathResolve(extendDir)));
+  }
+  if (!fs.existsSync(utils.pathResolve(publicDir))) {
+    return gulp.src('./excludes/public/**')
+      .pipe(gulp.dest(utils.pathResolve(publicDir)));
+  }
+  if (!fs.existsSync(utils.pathResolve(sourceDir))) {
     return gulp.src('./excludes/profiles/main/source/**')
-      .pipe(gulp.dest(utils.pathResolve(srcDir.root)));
+      .pipe(gulp.dest(utils.pathResolve(sourceDir)));
   }
   cb();
 });
 
 gulp.task('install:copy-base', function (cb) {
-  if (!fs.existsSync(utils.pathResolve(srcDir.root))) {
+  if (!fs.existsSync(utils.pathResolve(sourceDir))) {
     return gulp.src('./excludes/profiles/base/source/**')
-      .pipe(gulp.dest(utils.pathResolve(srcDir.root)));
+      .pipe(gulp.dest(utils.pathResolve(sourceDir)));
   }
   cb();
 });
