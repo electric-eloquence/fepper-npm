b01a716f59619bd534e7e0c29d2d0c8e913c7452
e2tha-e <ee@electric-eloquence.io>
2017-02-12 08:03:27 -0500

    accommodating ui corrections

--- core/tcp-ip/html-scraper-xhr.js
+++ core/tcp-ip/html-scraper-xhr.js
@@ -22,12 +22,15 @@ exports.main = function (req, res) {
     }
   }
 
-  output += '<link rel="stylesheet" href="/fepper-core/style.css" media="all">\n';
+  output += '<!DOCTYPE html>\n';
+  output += '<html>\n';
+  output += '<link rel="stylesheet" href="/fepper-core/style.css" media="all" />\n';
   output += '<section>\n';
   output += `<div class="${msgClass}">${message}</div>\n`;
   output += htmlObj.scraperTitle;
   output += htmlObj.landingBody;
   output += '</section>';
+  output += '</html>\n';
   output = output.replace('{{ message }}', message);
   output = output.replace('{{ url }}', url);
   output = output.replace('{{ target }}', target);
