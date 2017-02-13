0a2042f9265d49d44c9da869fdb6da358d10230f
e2tha-e <ee@electric-eloquence.io>
2017-02-13 09:44:38 -0500

    fixing firefox complaints about html scraper

--- core/tcp-ip/html-scraper.js
+++ core/tcp-ip/html-scraper.js
@@ -3,12 +3,13 @@
 const htmlObj = require('../lib/html');
 
 exports.main = function (req, res) {
-  var output = '';
+  let output = '';
 
   output += htmlObj.headWithMsg;
   output += '<script src="node_modules/fepper-ui/scripts/html-scraper-ajax.js"></script>\n';
   output += htmlObj.foot;
   output = output.replace('{{ title }}', 'Fepper HTML Scraper');
+  output = output.replace('{{ main_id }}', '');
   output = output.replace('{{ msg_class }}', '');
   output = output.replace('{{ message }}', '');
   res.end(output);
