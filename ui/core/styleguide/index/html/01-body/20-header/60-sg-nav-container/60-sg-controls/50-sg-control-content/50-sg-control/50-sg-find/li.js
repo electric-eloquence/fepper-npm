43f116ffe054511d465bf729583ef2f941908d5e
e2tha-e <ee@electric-eloquence.io>
2017-04-23 08:16:38 -0400

    ui toolbar style

--- ui/core/styleguide/index/html/01-body/20-header/60-sg-nav-container/60-sg-controls/50-sg-control-content/50-sg-control/50-sg-find/li.js
+++ ui/core/styleguide/index/html/01-body/20-header/60-sg-nav-container/60-sg-controls/50-sg-control-content/50-sg-control/50-sg-find/li.js
@@ -4,7 +4,7 @@ module.exports = {
     __html: `
 {{^ ishControlsHide.find }}
   <a href="#" class="sg-acc-handle sg-control-trigger sg-icon sg-icon-search" id="sg-f-toggle" title="Search Patterns"><span class="is-vishidden">Search Patterns</span></a>
-  <ul class="sg-view sg-acc-panel sg-right sg-checklist" id="sg-find" style="margin-top: 33px;">
+  <ul class="sg-view sg-acc-panel sg-right sg-checklist" id="sg-find" style="top: 32px;">
       <li><input class="typeahead" id="typeahead" type="text" placeholder="search for a pattern..."></li>
   </ul>
 {{/ ishControlsHide.find }}
