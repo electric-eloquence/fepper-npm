516caedf4272a0c38685e49cd5e8b01b99971779
e2tha-e <ee@electric-eloquence.io>
2017-04-27 21:10:50 -0400

    fp help text

--- tasker/help.js
+++ tasker/help.js
@@ -11,18 +11,18 @@ Usage:
 
 Tasks:
     fp                  Launch Fepper and open it in a browser.
-    fp data             Force-compile data.json from underscore-prefixed .json files.
+    fp data             Compile data.json from underscore-prefixed .json files.
     fp frontend-copy    Copy assets, scripts, and styles to the backend.
     fp once             Clean the public folder and do a one-off Fepper build.
     fp restart          Restart after shutdown, but without opening the browser.
     fp static           Generate a static site from the 04-pages directory.
     fp syncback         Combines frontend-copy and template tasks.
     fp template         Translate templates in 03-templates for the backend and copy them there.
-    fp ui:compile       Compile the React components composing the UI. 
+    fp ui:compile       Compile the UI React components.
     fp update           Update Fepper NPM, Fepper UI, and Fepper extensions.
     fp version          Print versions of Fepper CLI, Fepper NPM, and Fepper UI.
 
-Note: Windows users will need to recognize \`fp\` as an alias for \`cscript .\\fepper.vbs\` or \`.\\fepper.ps1\`
+Note: Windows users not using BASH will need to recognize `fp` as an alias for `cscript .\fepper.vbs` or `.\fepper.ps1`
 `;
   utils.info(out);
   cb();
