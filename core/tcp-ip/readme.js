10435618853e7e01ba14cbdcf088fa91563f0830
e2tha-e <ee@electric-eloquence.io>
2017-10-29 15:23:39 -0400

    Squashed commit of the following:
    commit 48ff90a1e6f39776c1a568a4cd2c586e82f83fc4
    Author: e2tha-e <ee@electric-eloquence.io>
    Date:   Mon Oct 16 10:02:08 2017 -0400
    
        better error handling on readme and success pages
    
    commit 8d64bafc9e4090a428c73b58f3e7ba474c292371
    Author: e2tha-e <ee@electric-eloquence.io>
    Date:   Mon Oct 16 09:52:14 2017 -0400
    
        try/catches for marked
    
    commit 3a40b3581a16c899f5716b2e0306925d1d4a2c00
    Author: e2tha-e <ee@electric-eloquence.io>
    Date:   Mon Oct 16 08:41:24 2017 -0400
    
        marked -> 8fold-marked

--- core/tcp-ip/readme.js
+++ core/tcp-ip/readme.js
@@ -10,9 +10,29 @@ const conf = global.conf;
 
 exports.main = function (req, res) {
   fs.readFile(utils.pathResolve('README.md'), conf.enc, function (err, dat) {
-    var htmlMd = marked(dat);
-    var output = htmlObj.head;
+    if (err) {
+      utils.error(err);
+    }
 
+    if (!dat) {
+      res.end(utils.httpCodes['404']);
+
+      return;
+    }
+
+    let htmlMd;
+
+    try {
+      htmlMd = marked(dat);
+    }
+    catch (err) {
+      utils.error(err);
+      res.end(err);
+
+      return;
+    }
+
+    let output = htmlObj.head;
     output += htmlMd + '\n';
     output += htmlObj.foot;
     output = output.replace('{{ title }}', 'Fepper');
