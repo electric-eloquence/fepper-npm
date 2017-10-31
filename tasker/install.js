c69c72ba7f53d662f42eb060aac0f2633ce3afae
e2tha-e <ee@electric-eloquence.io>
2017-10-31 06:39:46 -0400

    specifying windows bin when necessary

--- tasker/install.js
+++ tasker/install.js
@@ -6,6 +6,7 @@
 const spawnSync = require('child_process').spawnSync;
 const fs = require('fs-extra');
 const gulp = require('gulp');
+const slash = require('slash');
 
 const utils = require('../core/lib/utils');
 
@@ -13,6 +14,13 @@ const cwd = process.cwd();
 const extendDir = utils.pathResolve(global.conf.extend_dir);
 const publicDir = utils.pathResolve(global.conf.ui.paths.public.root);
 const sourceDir = utils.pathResolve(global.conf.ui.paths.source.root);
+
+let binNpm = 'npm';
+
+// Spawn npm.cmd if Windows and not BASH.
+if (process.env.ComSpec === 'C:\\WINDOWS\\system32\\cmd.exe') {
+  binNpm = 'npm.cmd';
+}
 
 gulp.task('install:copy', cb => {
   // Copy source dir if it doesn't exist.
@@ -29,20 +37,19 @@ gulp.task('install:copy', cb => {
   if (!fs.existsSync(`${extendDir}/node_modules`)) {
     process.chdir(extendDir);
     utils.log(`Working directory changed to ${extendDir}.`);
-    spawnSync('npm', ['install'], {stdio: 'inherit'});
+    spawnSync(binNpm, ['install'], {stdio: 'inherit'});
   }
 
   // Run npm install in public dir if no public/node_modules dir.
   if (!fs.existsSync(`${publicDir}/node_modules`)) {
-    process.chdir(cwd);
     process.chdir(publicDir);
     utils.log(`Working directory changed to ${publicDir}.`);
-    spawnSync('npm', ['install'], {stdio: 'inherit'});
+    spawnSync(binNpm, ['install'], {stdio: 'inherit'});
   }
 
   // Finish up.
   process.chdir(cwd);
-  utils.log(`Working directory changed to ${cwd}.`);
+  utils.log(`Working directory changed to ${slash(cwd)}.`);
 
   cb();
 });
