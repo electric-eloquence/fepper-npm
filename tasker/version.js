6432c567bbb2a1986a41fc683c3ec6b172a0b928
e2tha-e <ee@electric-eloquence.io>
2017-11-01 06:37:32 -0400

    using windows bin when necessary

--- tasker/version.js
+++ tasker/version.js
@@ -7,9 +7,16 @@ const utils = require('../core/lib/utils');
 
 const publicDir = utils.pathResolve(global.conf.ui.paths.public.root);
 
+let binNpm = 'npm';
+
+// Spawn npm.cmd if Windows and not BASH.
+if (process.env.ComSpec && process.env.ComSpec.toLowerCase() === 'c:\\windows\\system32\\cmd.exe') {
+  binNpm = 'npm.cmd';
+}
+
 gulp.task('version', cb => {
   // Output fepper-cli version.
-  const npmLsGlobal = spawn('npm', ['ls', '-g', 'fepper-cli', '--depth=0'], {stdio: 'inherit'});
+  const npmLsGlobal = spawn(binNpm, ['ls', '-g', 'fepper-cli', '--depth=0'], {stdio: 'inherit'});
 
   npmLsGlobal.on('error', err => {
     utils.error(err);
@@ -18,7 +25,7 @@ gulp.task('version', cb => {
   // Output fepper version.
   process.chdir(global.rootDir);
 
-  const npmLsFepper = spawn('npm', ['ls', 'fepper', '--depth=0'], {stdio: 'inherit'});
+  const npmLsFepper = spawn(binNpm, ['ls', 'fepper', '--depth=0'], {stdio: 'inherit'});
 
   npmLsFepper.on('error', err => {
     utils.error(err);
@@ -27,7 +34,7 @@ gulp.task('version', cb => {
   // Output fepper-ui version.
   process.chdir(publicDir);
 
-  const npmLsFepperUi = spawn('npm', ['ls', 'fepper-ui', '--depth=0'], {stdio: 'inherit'});
+  const npmLsFepperUi = spawn(binNpm, ['ls', 'fepper-ui', '--depth=0'], {stdio: 'inherit'});
 
   npmLsFepperUi.on('error', err => {
     utils.error(err);
