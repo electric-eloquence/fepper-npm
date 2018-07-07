b3aec62fb2486a8723848e80869ad742545a30b3
e2tha-e <ee@electric-eloquence.io>
2018-07-07 06:33:01 -0400

    replacing var with const and let in core/tasks/templater.js

--- core/tasks/templater.js
+++ core/tasks/templater.js
@@ -22,14 +22,15 @@ const patternsDir = utils.pathResolve(conf.ui.paths.source.patterns);
 const srcDir = utils.pathResolve(conf.ui.paths.source.templates);
 
 exports.mustacheRecurse = function (file) {
-  var code;
-  var code1 = '';
+  let code;
+  let code1 = '';
 
   try {
     // Read code which will receive token replacement.
     code = fs.readFileSync(file, conf.enc);
     // Split by Mustache tag for parsing.
     let codeSplit = code.split('{{');
+
     for (let i = 0; i < codeSplit.length; i++) {
       // Signal the OK to recurse by appending partial tags with the .mustache
       // extension. We do NOT want to recurse EVERY included partial because
@@ -40,9 +41,11 @@ exports.mustacheRecurse = function (file) {
         partial[0] = partial[0].replace(/^>\s*/, '').trim();
         let partialCode = exports.mustacheRecurse(patternsDir + '/' + partial[0], patternsDir);
         code1 += partialCode;
+
         for (let j = 0; j < partial.length; j++) {
           if (j > 0) {
             code1 += partial[j];
+
             if (j < partial.length - 1) {
               code1 += '}}';
             }
@@ -67,24 +70,24 @@ exports.mustacheRecurse = function (file) {
 };
 
 exports.mustacheUnescape = function (escaped) {
-  var unescaped = escaped.replace(/\{\s*/, '{\\s');
+  let unescaped = escaped.replace(/\{\s*/, '{\\s');
   unescaped = unescaped.replace(/\s*\}/, '\\s}');
 
   return unescaped;
 };
 
 exports.templateProcess = function (file, templatesDirDefault, templatesExtDefault) {
-  var code;
-  var data = null;
-  var dest;
-  var mustacheFile;
-  var srcDirParam = srcDir;
-  var stat = null;
-  var stat1 = null;
-  var templatesDir = '';
-  var templatesExt = '';
-  var yml;
-  var ymlFile = '';
+  let code;
+  let data = null;
+  let dest;
+  let mustacheFile;
+  let srcDirParam = srcDir;
+  let stat = null;
+  let stat1 = null;
+  let templatesDir = '';
+  let templatesExt = '';
+  let yml;
+  let ymlFile = '';
 
   // Exclude files prefixed by __
   if (path.basename(file).slice(0, 2) === '__') {
@@ -129,17 +132,20 @@ exports.templateProcess = function (file, templatesDirDefault, templatesExtDefau
 
       if (typeof data.templates_dir === 'string') {
         templatesDir = utils.backendDirCheck(data.templates_dir);
+
         // Do not maintain nested directory structure in backend if
         // templates_dir is set in exceptional YAML file.
         if (templatesDir) {
           srcDirParam = path.dirname(mustacheFile);
         }
+
         // Unset templates_dir in local YAML data.
         delete data.templates_dir;
       }
 
       if (typeof data.templates_ext === 'string') {
         templatesExt = utils.extCheck(data.templates_ext);
+
         // Unset templates_dir in local YAML data.
         delete data.templates_ext;
       }
@@ -172,15 +178,15 @@ exports.templateProcess = function (file, templatesDirDefault, templatesExtDefau
 };
 
 exports.templatesGlob = function () {
-  var globbed = glob.sync(srcDir + '/*.mustache');
-  var globbed1 = glob.sync(srcDir + '/!(_nosync)/**/*.mustache');
+  const globbed = glob.sync(srcDir + '/*.mustache');
+  const globbed1 = glob.sync(srcDir + '/!(_nosync)/**/*.mustache');
 
   return globbed.concat(globbed1);
 };
 
 exports.templatesWrite = function (file, srcDirParam, templatesDir, templatesExt, code) {
   // Determine destination for token-replaced code.
-  var dest = file.replace(srcDirParam, '');
+  let dest = file.replace(srcDirParam, '');
 
   // Replace underscore prefixes.
   dest = dest.replace(/\/_([^\/]+)$/, '/$1');
@@ -195,10 +201,10 @@ exports.templatesWrite = function (file, srcDirParam, templatesDir, templatesExt
 };
 
 exports.tokensReplace = function (tokens, codeParam) {
-  var code = codeParam;
-  var regex;
-  var token;
-  var unescaped;
+  let code = codeParam;
+  let regex;
+  let token;
+  let unescaped;
 
   for (let i in tokens) {
     if (tokens.hasOwnProperty(i)) {
@@ -222,9 +228,9 @@ exports.tokensReplace = function (tokens, codeParam) {
 };
 
 exports.main = function () {
-  var files = exports.templatesGlob();
-  var templatesDirDefault = utils.backendDirCheck(pref.backend.synced_dirs.templates_dir);
-  var templatesExtDefault = utils.extCheck(pref.backend.synced_dirs.templates_ext);
+  const files = exports.templatesGlob();
+  const templatesDirDefault = utils.backendDirCheck(pref.backend.synced_dirs.templates_dir);
+  const templatesExtDefault = utils.extCheck(pref.backend.synced_dirs.templates_ext);
 
   // Search source directory for Mustache files.
   // Excluding templates in _nosync directory and those prefixed by __.
