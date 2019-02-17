814995e07d549391776940e218f563ec1dd09e60
e2tha-e <ee@electric-eloquence.io>
2019-02-17 07:01:24 -0500

    reinstating turquoise color to log messages

--- core/tasks/templater.js
+++ core/tasks/templater.js
@@ -175,7 +175,7 @@ module.exports = class {
         // Write translated templates.
         const dest = this.templatesWrite(mustacheFile, srcDirParam, templatesDir, templatesExt, code);
 
-        this.utils.log('Template %s translated.', dest.replace(this.rootDir, '').replace(/^\//, ''));
+        this.utils.log('Template \x1b[36m%s\x1b[0m translated.', dest.replace(this.rootDir, '').replace(/^\//, ''));
       }
       catch (err) {
         this.utils.error(err);
