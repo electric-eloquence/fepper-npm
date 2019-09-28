36ecccc50d64aeae7b45c44fe7569852e8315237
e2tha-e <ee@electric-eloquence.io>
2019-09-27 22:17:17 -0400

    Revert "adjustment to global.rootDir for ui-compiler-tests.js to work"
    This reverts commit 2fbf3f0ad55632ded85fbc28e834c59fd312977e.

--- ui/test/tests/ui-compiler-tests.js
+++ ui/test/tests/ui-compiler-tests.js
@@ -3,15 +3,9 @@
 const {expect} = require('chai');
 const fs = require('fs-extra');
 
-// Need to unset and reset global.rootDir for this test to work alongside other tests.
-const rootDir = global.rootDir;
-delete global.rootDir;
-
 const {
   patternlab
 } = require('../init')();
-
-global.rootDir = rootDir;
 
 describe('UI Compiler', function () {
   const fepletFile = `${patternlab.config.paths.public.styleguide}/node_modules/feplet/dist/feplet.browser.es6.min.js`;
