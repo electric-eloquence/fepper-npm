bea82b0156310aa3e21e2bb9342421833147c55b
e2tha-e <ee@electric-eloquence.io>
2020-02-01 09:33:07 -0500

    comment updates

--- ui/core/lib/ui-builder.js
+++ ui/core/lib/ui-builder.js
@@ -19,7 +19,7 @@ module.exports = class {
     this.utils = patternlab.utils;
   }
 
-  // Getters for patternlab instance props in case they are undefined at instantiation.
+  // GETTERS for patternlab instance props in case they are undefined at instantiation.
 
   get patternBuilder() {
     return this.#patternlab.patternBuilder;
