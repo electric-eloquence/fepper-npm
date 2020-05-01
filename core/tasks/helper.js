41181edba2dd06a9c037a576181bcace827b3176
e2tha-e <ee@electric-eloquence.io>
2020-05-01 07:07:46 -0400

    updated messages in source/_ui/i18n/en.json

--- core/tasks/helper.js
+++ core/tasks/helper.js
@@ -26,12 +26,12 @@ ${t('Tasks:')}
     fp restart          ${t('Restart after shutdown, but without opening the browser')}
     fp static           ${t('Generate a static site from the 04-pages directory')}
     fp syncback         ${t('Combine frontend-copy and template tasks')}
-    fp template         ${t('Translate templates in 03-templates for the backend, and copy them there')}
+    fp template         ${t('Translate templates in 03-templates for the backend, and output them there')}
     fp ui:compile       ${t('Compile the UI React components')}
-    fp ui:help          ${t('Print UI tasks and descriptions')}
+    fp ui:help          ${t('Print UI tasks and their descriptions')}
     fp update           ${t('Update Fepper distro, Fepper CLI, Fepper NPM, Fepper UI, and Fepper extensions')}
-    fp version          ${t('Print versions of Fepper CLI, Fepper NPM, and Fepper UI')}
-    fp extend:help      ${t('Print extension tasks and descriptions')}
+    fp version          ${t('Print the versions of Fepper CLI, Fepper NPM, and Fepper UI')}
+    fp extend:help      ${t('Print extension tasks and their descriptions')}
 `;
 
     this.utils.info(out);
