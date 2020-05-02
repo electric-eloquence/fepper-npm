dec782ef2873646c49a2d7bf15abefa23b4484d7
e2tha-e <ee@electric-eloquence.io>
2020-05-02 07:30:33 -0400

    updated icomoon fonts

--- ui/core/styleguide/index/html/01-body/20-header/60-sg-controls/60-sg-view/li.component.js
+++ ui/core/styleguide/index/html/01-body/20-header/60-sg-controls/60-sg-view/li.component.js
@@ -5,9 +5,9 @@ module.exports = {
 {{^ ishControlsHide.views-all }}
   <a href="#" class="sg-acc-handle sg-control-trigger sg-icon sg-icon-eye" id="sg-t-toggle" title="${t('View')}"><span class="is-vishidden">${t('View')}</span></a>
   <ul class="sg-acc-panel sg-right sg-checklist" id="sg-view">
-    {{^ ishControlsHide.views-annotations }}<li><a href="#" class="sg-checklist-icon sg-checkbox" id="sg-t-annotations">${t('Annotations')}</a></li>{{/ ishControlsHide.views-annotations }}
-    {{^ ishControlsHide.views-code }}<li><a href="#" class="sg-checklist-icon sg-checkbox" id="sg-t-code">${t('Code')}</a></li>{{/ ishControlsHide.views-code }}
-    {{^ ishControlsHide.views-new }}<li><a href="#" target="_blank" id="sg-raw" class="sg-checklist-icon sg-icon-link">${t('Open in new window')}</a></li>{{/ ishControlsHide.views-new }}
+    {{^ ishControlsHide.views-annotations }}<li><a href="#" class="sg-checklist-icon sg-icon sg-icon-radio" id="sg-t-annotations">${t('Annotations')}</a></li>{{/ ishControlsHide.views-annotations }}
+    {{^ ishControlsHide.views-code }}<li><a href="#" class="sg-checklist-icon sg-icon sg-icon-radio" id="sg-t-code">${t('Code')}</a></li>{{/ ishControlsHide.views-code }}
+    {{^ ishControlsHide.views-new }}<li><a href="#" target="_blank" id="sg-raw" class="sg-checklist-icon sg-icon sg-icon-link">${t('Open in new window')}</a></li>{{/ ishControlsHide.views-new }}
   </ul>
 {{/ ishControlsHide.views-all }}
 `
