460cb7bd2ce79b2a73a6890abdc30a1cf60f7806
e2tha-e <ee@electric-eloquence.io>
2020-05-10 18:21:47 -0400

    updated i18n messages

--- ui/core/styleguide/index/html/01-body/20-header/60-sg-controls/40-sg-find/li.component.js
+++ ui/core/styleguide/index/html/01-body/20-header/60-sg-controls/40-sg-find/li.component.js
@@ -3,9 +3,9 @@ module.exports = {
   dangerouslySetInnerHTML: {
     __html: `
 {{^ ishControlsHide.find }}
-  <a href="#" class="sg-control-trigger sg-icon sg-icon-search" id="sg-f-toggle" title="${t('Search Patterns')}"><span class="is-vishidden">${t('Search Patterns')}</span></a>
+  <a href="#" class="sg-control-trigger sg-icon sg-icon-search" id="sg-f-toggle" title="${t('Search patterns')}"><span class="is-vishidden">${t('Search patterns')}</span></a>
   <ul class="sg-acc-panel sg-right sg-checklist" id="sg-find" style="top: 32px;">
-    <li><input class="typeahead" id="typeahead" type="text" placeholder="${t('Search Patterns')}"></li>
+    <li><input class="typeahead" id="typeahead" type="text" placeholder="${t('Search patterns')}"></li>
   </ul>
 {{/ ishControlsHide.find }}
 `
