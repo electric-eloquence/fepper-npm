b3212734abcc7f273fff119598619d92340c2ba1
e2tha-e <ee@electric-eloquence.io>
2020-05-10 18:21:47 -0400

    updated i18n messages

--- ui/core/styleguide/index/html/01-body/20-header/60-sg-controls/80-sg-tools/li.component.js
+++ ui/core/styleguide/index/html/01-body/20-header/60-sg-controls/80-sg-tools/li.component.js
@@ -5,9 +5,9 @@ module.exports = {
 {{^ ishControlsHide.tools-all }}
   <a href="#" class="sg-acc-handle sg-control-trigger sg-icon sg-icon-cog" id="sg-tools-toggle" title="${t('Tools')}"><span class="is-vishidden">${t('Tools')}</span></a>
   <ul class="sg-acc-panel sg-right sg-checklist" id="sg-tools">
-    {{^ ishControlsHide.tools-fpdocs }}<li><a href="/readme" class="sg-tool sg-checklist-icon sg-icon sg-icon-file" target="_blank">${t('Fepper Docs')}</a>{{/ ishControlsHide.tools-fpdocs }}
-    {{^ ishControlsHide.tools-docs }}<li><a href="https://patternlab.io/docs/" class="sg-tool sg-checklist-icon sg-icon sg-icon-file" target="_blank">${t('Pattern Lab Docs')}</a>{{/ ishControlsHide.tools-docs }}
-    {{^ ishControlsHide.tools-shortcuts }}<li><a href="/readme#keyboard-shortcuts" class="sg-tool sg-checklist-icon sg-icon sg-icon-keyboard" target="_blank">${t('Keyboard Shortcuts')}</a>{{/ ishControlsHide.tools-shortcuts }}
+    {{^ ishControlsHide.tools-fpdocs }}<li><a href="/readme" class="sg-tool sg-checklist-icon sg-icon sg-icon-file" target="_blank">${t('Fepper docs')}</a>{{/ ishControlsHide.tools-fpdocs }}
+    {{^ ishControlsHide.tools-docs }}<li><a href="https://patternlab.io/docs/" class="sg-tool sg-checklist-icon sg-icon sg-icon-file" target="_blank">${t('Pattern Lab docs')}</a>{{/ ishControlsHide.tools-docs }}
+    {{^ ishControlsHide.tools-shortcuts }}<li><a href="/readme#keyboard-shortcuts" class="sg-tool sg-checklist-icon sg-icon sg-icon-keyboard" target="_blank">${t('Keyboard shortcuts')}</a>{{/ ishControlsHide.tools-shortcuts }}
   </ul>
 {{/ ishControlsHide.tools-all }}
 `
