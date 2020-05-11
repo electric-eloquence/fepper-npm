17b4820ee653447e4b0c6b0e990db9dec254857a
e2tha-e <ee@electric-eloquence.io>
2020-05-11 08:07:51 -0400

    improved html scraper functionality

--- core/lib/html.js
+++ core/lib/html.js
@@ -24,6 +24,7 @@ exports.head = `
     {{{ patternlabHead }}}
 
     <link rel="stylesheet" href="/fepper-core/style.css" media="all">
+    <script src="../../node_modules/mousetrap/mousetrap.min.js"></script>
   </head>
 
   <body class="text {{ body_class }}">
@@ -61,7 +62,7 @@ exports.landingBody = `
       <div id="help-text" style="border: 1px solid black;visibility: hidden;margin-top: 5.50px;padding: 0 20px;width: 100%">
         <p></p>
         <p>${t('Use this tool to scrape and import Mustache templates and JSON data files from actual web pages, preferably the actual backend CMS that Fepper is prototyping for. Simply enter the URL of the page you wish to scrape. Then, enter the CSS selector you wish to target (prepended with &quot;#&quot; for IDs and &quot;.&quot; for classes). Classnames and tagnames may be appended with array index notation ([n]). Otherwise, the Scraper will scrape all elements of that class or tag sequentially. Such a loosely targeted scrape will save many of the targeted fields to the JSON file, but will only save the first instance of the target to a Mustache template.')}</p>
-  <p>${t('Upon submit, you should be able to review the scraped output on the subsequent page. If the output looks correct, enter a filename and submit again. The Scraper will save Mustache and JSON files by that name in your patterns&apos; scrape directory, also viewable under the Scrape menu of the toolbar. The Scraper will correctly indent the Mustache code. However, the JSON parsing requires a conversion from HTML to XHTML, so don&apos;t expect an exact copy of the HTML structure of the source HTML.')}</p>
+        <p>${t('Upon submit, you should be able to review the scraped output on the subsequent page. If the output looks correct, enter a filename and submit again. The Scraper will save Mustache and JSON files by that name in your patterns&apos; scrape directory, also viewable under the Scrape menu of the toolbar. The Scraper will correctly indent the Mustache code. However, the parsing may render an inexact copy of your source HTML.')}</p>
       </div>`;
 
 exports.reviewerPrefix = `
