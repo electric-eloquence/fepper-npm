175f2dd8db10d011d1f2b596eba36c69b4f7172e
e2tha-e <ee@electric-eloquence.io>
2020-06-15 17:14:04 -0400

    .is-vishidden renamed to .visually-hidden

--- ui/core/styleguide/index/html/01-body/20-header/60-sg-controls/60-sg-view/li.component.js
+++ ui/core/styleguide/index/html/01-body/20-header/60-sg-controls/60-sg-view/li.component.js
@@ -3,7 +3,7 @@ module.exports = {
   dangerouslySetInnerHTML: {
     __html: `
 {{^ ishControlsHide.views-all }}
-  <a href="#" class="sg-acc-handle sg-control-trigger sg-icon sg-icon-eye" id="sg-t-toggle" title="${t('View')}"><span class="is-vishidden">${t('View')}</span></a>
+  <a href="#" class="sg-acc-handle sg-control-trigger sg-icon sg-icon-eye" id="sg-t-toggle" title="${t('View')}"><span class="visually-hidden">${t('View')}</span></a>
   <ul class="sg-acc-panel sg-right sg-checklist" id="sg-view">
     {{^ ishControlsHide.views-annotations }}<li><a href="#" class="sg-checklist-icon sg-icon sg-icon-radio" id="sg-t-annotations">${t('Annotations')}</a></li>{{/ ishControlsHide.views-annotations }}
     {{^ ishControlsHide.views-code }}<li><a href="#" class="sg-checklist-icon sg-icon sg-icon-radio" id="sg-t-code">${t('Code')}</a></li>{{/ ishControlsHide.views-code }}
