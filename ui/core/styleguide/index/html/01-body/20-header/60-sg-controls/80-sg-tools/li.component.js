0ea513ce3fbe2ee8d14bd93a163c525a235b137c
electric-el <electric-el@electric-eloquence.io>
2021-01-14 07:53:11 -0500

    minor html improvement

--- ui/core/styleguide/index/html/01-body/20-header/60-sg-controls/80-sg-tools/li.component.js
+++ ui/core/styleguide/index/html/01-body/20-header/60-sg-controls/80-sg-tools/li.component.js
@@ -5,8 +5,8 @@ module.exports = {
 {{^ ishControlsHide.tools-all }}
   <a href="#" class="sg-acc-handle sg-control-trigger sg-icon sg-icon-cog" id="sg-tools-toggle" title="${t('Tools')}"><span class="visually-hidden">${t('Tools')}</span></a>
   <ul class="sg-acc-panel sg-right sg-checklist" id="sg-tools">
-    {{^ ishControlsHide.tools-docs }}<li><a href="https://fepper.io/docpage" class="sg-tool sg-checklist-icon sg-icon sg-icon-file" target="_blank">${t('Fepper docs')}</a>{{/ ishControlsHide.tools-docs }}
-    {{^ ishControlsHide.tools-shortcuts }}<li><a href="/readme#keyboard-shortcuts" class="sg-tool sg-checklist-icon sg-icon sg-icon-keyboard" target="_blank">${t('Keyboard shortcuts')}</a>{{/ ishControlsHide.tools-shortcuts }}
+    {{^ ishControlsHide.tools-docs }}<li><a href="https://fepper.io/docpage" class="sg-tool sg-checklist-icon sg-icon sg-icon-file" target="_blank">${t('Fepper docs')}</a></li>{{/ ishControlsHide.tools-docs }}
+    {{^ ishControlsHide.tools-shortcuts }}<li><a href="/readme#keyboard-shortcuts" class="sg-tool sg-checklist-icon sg-icon sg-icon-keyboard" target="_blank">${t('Keyboard shortcuts')}</a></li>{{/ ishControlsHide.tools-shortcuts }}
   </ul>
 {{/ ishControlsHide.tools-all }}
 `
