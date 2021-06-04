cc073df91e867dbf8f07c9a8021a0f3835f9a260
electric-el <electric-el@electric-eloquence.io>
2021-06-03 21:06:14 -0400

    added deprecation messages for annotations and code viewers

--- ui/core/styleguide/index/html/01-body/60-aside/25-annotations-template/div.component.js
+++ ui/core/styleguide/index/html/01-body/60-aside/25-annotations-template/div.component.js
@@ -3,13 +3,26 @@ module.exports = {
   id: 'sg-annotations-container',
   dangerouslySetInnerHTML: {
     __html: `
-  <div id="sg-annotations">
-    <div class="sg-annotation">
-      <h2>${t('No Annotations')}</h2>
-      <div>${t('There are no annotations for this pattern')}</div>
-    </div>
+<div id="sg-annotations">
+  <div class="sg-annotation">
+    <h2>${t('No Annotations')}</h2>
+    <div>${t('There are no annotations for this pattern')}</div>
   </div>
 </div>
+<!-- DEPRECATED SCRIPT FOR BACKWARD-COMPATIBILITY. TO BE REMOVED. -->
+<script>
+  const sgVpWrap = document.getElementById('sg-vp-wrap');
+
+  const deprecationMessage = document.createElement('code');
+  deprecationMessage.innerHTML = 'The annotations viewer requires Fepper-NPM to be updated in order to work properly.';
+  deprecationMessage.style.color = 'red';
+  deprecationMessage.style.position = 'absolute';
+  deprecationMessage.style.top = 'calc(50% + 2rem)';
+  deprecationMessage.style.left = '3.2rem';
+  deprecationMessage.style.zIndex = '-1';
+
+  sgVpWrap.appendChild(deprecationMessage);
+</script>
 `
   }
 };
