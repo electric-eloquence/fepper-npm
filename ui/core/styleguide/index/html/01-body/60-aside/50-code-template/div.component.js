d2c038524262b5d2ce95897e7987147366241314
electric-el <electric-el@electric-eloquence.io>
2021-06-06 08:45:04 -0400

    reversed requerio and git positions on code viewer

--- ui/core/styleguide/index/html/01-body/60-aside/50-code-template/div.component.js
+++ ui/core/styleguide/index/html/01-body/60-aside/50-code-template/div.component.js
@@ -23,8 +23,8 @@ module.exports = {
 <ul id="sg-code-tabs">
   <li id="sg-code-tab-feplet" class="sg-code-tab sg-code-tab-active" title="${t('Browse Fepper\'s Mustache templates')}">Feplet</li>
   <li id="sg-code-tab-markdown" class="sg-code-tab" title="${t('Edit this pattern\'s Markdown content')}">Markdown</li>
-  <li id="sg-code-tab-requerio" class="sg-code-tab" title="${t('Time-travel through Fepper\'s application states')}">Requerio</li>
   <li id="sg-code-tab-git" class="sg-code-tab" title="${t('Download and upload revisions to this project')}">Git</li>
+  <li id="sg-code-tab-requerio" class="sg-code-tab" title="${t('Time-travel through Fepper\'s application states')}">Requerio</li>
 </ul>
 <div id="sg-code-panels">
   <iframe id="sg-code-panel-feplet" class="sg-code-panel sg-code-panel-active" sandbox="allow-same-origin allow-scripts allow-top-navigation"></iframe>
@@ -42,8 +42,8 @@ module.exports = {
       <button id="sg-code-btn-markdown-save" class="sg-code-btn" title="${t('Save Markdown content')}">${t('Save')}</button>
     </div>
   </div>
-  <div id="sg-code-panel-requerio" class="sg-code-panel"></div>
   <div id="sg-code-panel-git" class="sg-code-panel"></div>
+  <div id="sg-code-panel-requerio" class="sg-code-panel"></div>
 </div>
 `
   }
