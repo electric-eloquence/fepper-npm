0decf139aa7e865155b92157dc1ab504add44438
electric-el <electric-el@electric-eloquence.io>
2021-12-15 17:33:12 -0500

    text update

--- ui/core/styleguide/index/html/01-body/60-aside/50-code-template/div.component.js
+++ ui/core/styleguide/index/html/01-body/60-aside/50-code-template/div.component.js
@@ -29,7 +29,7 @@ module.exports = {
   <li id="sg-code-tab-feplet" class="sg-code-tab sg-code-tab-active" title="${t('Browse Fepper\'s Mustache templates')}">Feplet</li>
   <li id="sg-code-tab-markdown" class="sg-code-tab" title="${t('Edit this pattern\'s Markdown content')}">Markdown</li>
   <li id="sg-code-tab-git" class="sg-code-tab" title="${t('Download and upload revisions to this project')}">Git</li>
-  <li id="sg-code-tab-requerio" class="sg-code-tab" title="${t('Time-travel through this pattern\'s application states')}">Requerio</li>
+  <li id="sg-code-tab-requerio" class="sg-code-tab" title="${t('Inspect this pattern\'s Requerio states')}">Requerio</li>
 </ul>
 <div id="sg-code-panels">
   <iframe id="sg-code-panel-feplet" class="sg-code-panel sg-code-panel-active" sandbox="allow-same-origin allow-scripts allow-top-navigation"></iframe>
